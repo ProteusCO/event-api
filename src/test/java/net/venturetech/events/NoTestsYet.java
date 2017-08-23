2fe4361e97b66a3721f2d3b220d8dc2aafed7d71
Justin Piper <jpiper@venturetech.net>
2017-08-23 15:36:37 -0500

    Remove unused starter app files

---
+++ src/test/java/net/venturetech/events/NoTestsYet.java
@@ -0,0 +1,12 @@
+package net.venturetech.events;
+
+import org.testng.annotations.*;
+
+public class NoTestsYet
+{
+    @Test
+    public void noTestsYet()
+    {
+        assert false: "No tests yet";
+    }
+}
